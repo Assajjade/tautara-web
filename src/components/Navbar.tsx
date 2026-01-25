61bfa6f3782037b305a5c3e120c7592b7af3737a
Assajjade <abdillah.assajjad@ui.ac.id>
2026-01-25 12:10:51 +0700

    feat: update Navbar component to hide account links on mobile and display menu icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,12 +18,17 @@ export default function Navbar() {
                     </div>
                 </div>
 
-                <div className='flex items-center gap-2'>
+                <div className='hidden md:flex items-center gap-2'>
                         <Link href="">Buat Akun</Link>
                         <span>|</span>
                         <Link href="">Masuk</Link>
                 </div>
 
+                <div className="md:hidden text-slate-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </div>
             </div>
         </nav>
     )
